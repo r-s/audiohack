88c753d2a71c8d16110c12351370925a1c59505f
biuh <biuh@biuh.de>
2012-06-19 17:23:52 +0200

    added source/Time.cpp

---
+++ source/Time.cpp
@@ -0,0 +1,21 @@
+// Time.cpp
+// created 19.06.12
+// by tobi
+
+// todo:
+// - everything
+
+#include <Time.h>
+
+void Time::cutAtTime(doubel second) {
+	// todo:
+	// - everything
+
+}
+
+bool Time::cutAtContent(double second, double tolerance) {
+	// todo:
+	// - everything
+
+	return false;
+}
