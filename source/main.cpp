3fdd8d8029cd44f6a0de2e55da06207c4bdffaac
r-s <ralf.strecker@googlemail.com>
2012-10-30 13:59:45 +0100

    initial implementation of user interface (no error handling yet); initial documentation of program errors

--- source/main.cpp
+++ source/main.cpp
@@ -13,7 +13,7 @@ using namespace std;
 int main (int argc, char* argv[])
 {
     
-    /*
+    
     if (argc < 3) {
         cout << "Use: audiohack inputfilepath function [parameter 1] [parameter 2])" << endl << "For further documentation see user_interface.pdf" << endl;
         return 1;
@@ -24,43 +24,47 @@ int main (int argc, char* argv[])
     
     //cout << filepath << endl << function << endl;
     
-     if (function == "-rev"){
+     if (!strcmp(function,"-rev")){
          Superclass rev(inputfilepath);
          rev.reverse();
      }
-     else if (function == "-phase"){
+     else if (!strcmp(function,"-phase")){
          Superclass phase(inputfilepath);
          phase.invertPhase();
      }
-     else if (function == "-norm"){
-         Dynamics norm(inputfilepath);
-         norm.normalize();
-     }
-     else if (function == "-gain"){
-         Dynamics gain(inputfilepath);
-         gain.gain(*argv[3]);
-     }
-     else if (function == "-fadein"){
+    
+// THIS WILL CAUSE BUILD ERRORS, BECAUSE NORMALIZE AND GAIN ARE NOT IMPLEMENTED YET!
+
+//     else if (!strcmp(function,"-norm")){
+//         Dynamics norm(inputfilepath);
+//         norm.normalize();
+//     }
+//     else if (!strcmp(function,"-gain")){
+//         Dynamics gain(inputfilepath);
+//         gain.gain(*argv[3]);
+//     }
+    
+     else if (!strcmp(function,"-fadein")){
          Time fadeIn(inputfilepath);
          fadeIn.fadeIn(*argv[3]);
      }
-     else if (function == "-fadeout"){
+     else if (!strcmp(function,"-fadeout")){
          Time fadeOut(inputfilepath);
          fadeOut.fadeOut(*argv[3]);
      }
-     else if (function == "-cut"){
+     else if (!strcmp(function,"-cut")){
          Time cut(inputfilepath);
          cut.cutAtTime(*argv[3]);
      }
-     else if (function == "-gate"){
+     else if (!strcmp(function,"-gate")){
          Dynamics gate(inputfilepath);
          gate.gatePipe(true, *argv[3], *argv[4], *argv[5]);
      }
-     else if (function == "-pipe"){
+     else if (!strcmp(function,"-pipe")){
          Dynamics pipe(inputfilepath);
          pipe.gatePipe(false, *argv[3], *argv[4], *argv[5]);
      }
-     else if (function == "-delay"){
+     else if (!strcmp(function,"-delay")){
          Effects delay(inputfilepath);
          delay.delay(*argv[3], *argv[4], *argv[5]);
      }
@@ -68,7 +72,7 @@ int main (int argc, char* argv[])
      cout << "Use: ./audiohack inputfilepath function [parameter 1] [parameter 2])" << endl << "For further documentation see user_interface.pdf" << endl;
      return 1;
      }
-     */
+     
 	
     return 0;
 }
